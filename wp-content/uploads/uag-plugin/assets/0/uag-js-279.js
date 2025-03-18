ed03e553e4e8c102875d7e44c2f5fb4676240597
Nere <kamildzielakdev@gmail.com>
2025-03-18 18:25:14 +0100

    Add files via upload

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-279.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-9fe039a9', {"layout":"number","heading":"Szcz\u0119\u015bliwi klienci","numberPrefix":"","numberSuffix":"+","startNumber":0,"endNumber":700,"totalNumber":700,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-8a8669eb', {"layout":"number","heading":"Nagrodzony","numberPrefix":"0","numberSuffix":"+","startNumber":0,"endNumber":8,"totalNumber":700,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":"","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-22ebbefe', {"layout":"number","heading":"Satysfakcja klienta","numberPrefix":"","numberSuffix":"%","startNumber":0,"endNumber":98,"totalNumber":700,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+ });
